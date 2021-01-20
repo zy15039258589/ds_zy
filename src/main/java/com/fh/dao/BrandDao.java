c8ec6824ad930d19ee33ce67ae2925ef50b0997b
zhengyang <zy15039258589@163.com>
2021-01-20 14:29:02 +0800

    修改了部门表的bug

--- src/main/java/com/fh/dao/BrandDao.java
+++ src/main/java/com/fh/dao/BrandDao.java
@@ -21,11 +21,11 @@ public interface BrandDao {
             "</script>")
     List<Brand> getBrandDateList(BrandParams params);
     @Update("update ds_brand set name=#{name},bandE=#{bandE},imgpath=#{imgpath}," +
-            "bandDesc=#{bandDesc},ord=#{ord},isdel=#{isdel},createDate=#{createDate},author=#{author}" +
+            "bandDesc=#{bandDesc},ord=#{ord},isdel=#{isdel},updateDate=#{updateDate},author=#{author}" +
             "   where id=#{id}")
     void updateBrand(Brand brand);
-    @Insert("insert into ds_brand (name,bandE,imgpath,bandDesc,ord,isdel,updateDate,author) value(" +
-            "#{name},#{bandE},#{imgpath},#{bandDesc},#{ord},#{isdel},#{updateDate},#{author} )")
+    @Insert("insert into ds_brand (name,bandE,imgpath,bandDesc,ord,isdel,createDate,author) value(" +
+            "#{name},#{bandE},#{imgpath},#{bandDesc},#{ord},#{isdel},#{createDate},#{author} )")
     void addBrand(Brand brand);
     @Update("update ds_brand set isdel=1 where id=#{id}")
     void delete(Integer id);
